c281bfa25763333dcedf41c115e73f32f5573fce
Justin Thompson <justindthomps@gmail.com>
2020-01-22 11:52:44 -0600

    init commit

---
+++ RainbowTabs/ViewControllers/GreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GreenViewController.swift
+//  RainbowTabs
+//
+//  Created by Thompson, Justin D on 1/22/20.
+//  Copyright © 2020 Thompson, Justin D. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RainbowTabs/ViewControllers/OrangeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OrangeViewController.swift
+//  RainbowTabs
+//
+//  Created by Thompson, Justin D on 1/22/20.
+//  Copyright © 2020 Thompson, Justin D. All rights reserved.
+//
+
+import UIKit
+class OrangeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = nil
+    }
+
+
+}
